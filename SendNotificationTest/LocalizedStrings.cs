0f6e43047ad4e978d75c5d7d4501d5dc9b02c55b
ostolz <oliver_stolz@web.de>
2014-08-01 14:45:59 +0200

    erstellung gitrepo

---
+++ SendNotificationTest/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using SendNotificationTest.Resources;
+
+namespace SendNotificationTest
+{
+    /// <summary>
+    /// Bietet Zugriff auf Zeichenfolgenressourcen.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ SendToNotificationHub/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Notifications;
+
+namespace SendToNotificationHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SendNotificationAsync();
+            Console.ReadLine();
+        }
+
+        private static async void SendNotificationAsync()
+        {
+            NotificationHubClient hub = NotificationHubClient
+                .CreateClientFromConnectionString("Endpoint=sb://ostolztest.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=GjACXVLUAmQ0MPc7Or4uvAVBTgnYzeBwqWo31oklg1s=", "alarm");
+            string toast = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
+                "<wp:Notification xmlns:wp=\"WPNotification\">" +
+                   "<wp:Toast>" +
+                        "<wp:Text1>Hello from a .NET App!</wp:Text1>" +
+                   "</wp:Toast> " +
+                "</wp:Notification>";
+            await hub.SendMpnsNativeNotificationAsync(toast);
+        }
+    }
+}
